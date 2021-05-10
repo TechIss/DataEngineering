f39ec0e4ddb11f0a0a6e4a506e68570ffeaef006
TechIss <issakarissefabiala@gmail.com>
2021-05-10 09:26:02 +0200

    Datanbase-order-by

--- database-beginner/03-level/queries.sql
+++ database-beginner/03-level/queries.sql
@@ -31,3 +31,21 @@ SELECT MAX(rating) FROM `series`;
 SELECT * FROM `series` where seasons<3 OR seasons>20;
 SELECT * FROM `series` where title LIKE '%Th%';
 SELECT * FROM `series` where seasons != 3;
+
+SELECT * FROM `series` 
+WHERE rating>2.5
+ORDER BY
+    rating DESC;
+
+SELECT * FROM `series` 
+WHERE seasons<5
+ORDER BY
+    seasons ASC;
+
+SELECT * FROM `series` 
+WHERE seasons<3 OR seasons>20
+ORDER BY
+    seasons AND country ASC
+
+
+
