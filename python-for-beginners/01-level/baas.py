731356f33388c1cab515756eb9ccbb90d29114c2
TechIss <issakarissefabiala@gmail.com>
2021-05-17 10:38:41 +0200

    feature-baas

---
+++ python-for-beginners/01-level/baas.py
@@ -0,0 +1,10 @@
+wachtwoord = input("wat is het wachtwoord? ")
+input = ("baas")
+
+i = ("Baas is ingelogd: True")
+x =  ("Baas is ingelogd: False")
+
+if (wachtwoord == input): 
+    print(i)
+else:
+    print(x)
