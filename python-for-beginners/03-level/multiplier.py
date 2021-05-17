f06f48752d4659f974ee629f2b412df046a27dc1
TechIss <issakarissefabiala@gmail.com>
2021-05-17 21:22:03 +0200

    feature-vermenigvuldigen

--- python-for-beginners/03-level/multiplier.py
+++ python-for-beginners/03-level/multiplier.py
@@ -1,6 +1,13 @@
-getal_1 = input("Eerste getal? ")
-getal_2 = input("Tweede getal? ")
+getal_1 = input("Eerste getal?")
+getal_2 = input("Tweede getal?")
 
-getalInput = input(float(getal_1) + float(getal_2))
+# getalInput = input(float(getal_1) + float(getal_2))
+# getalInput = input(float(getal_1) * float(getal_2))
 
 print("Resultaat: " + str(float(getal_1) + float(getal_2)))
+
+
+getal_3 = input("Eerste getal?")
+getal_4 = input("Tweede getal?")
+
+print("Resultaat: " + str(float(getal_3) * float(getal_4)))
