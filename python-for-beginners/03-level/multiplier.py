266d8a8570dac5b0482ab6adbfc9bbcc9dcb3c8f
TechIss <issakarissefabiala@gmail.com>
2021-05-17 21:11:16 +0200

    feature-multiplyer-level-03

---
+++ python-for-beginners/03-level/multiplier.py
@@ -0,0 +1,6 @@
+getal_1 = input("Eerste getal? ")
+getal_2 = input("Tweede getal? ")
+
+getalInput = input(float(getal_1) + float(getal_2))
+
+print("Resultaat: " + str(float(getal_1) + float(getal_2)))
